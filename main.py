ed9abc2351a80eaacb668c912c28b9bbdf0171c6
johanasplund <johanasplund@live.se>
2014-05-24 03:50:20 +0200

    removing files etc

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask import url_for
 from flask import render_template
-from get_id import get_new_video
+from get_new_video import get_new_video
 
 app = Flask(__name__)
 
