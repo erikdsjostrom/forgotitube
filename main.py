244f1f96f79b57643a1356f5e393f5971d9a6bce
Erik Sjöström <metallicgeek@gmail.com>
2014-05-24 14:38:01 +0200

    making progress on the advanced settings

--- main.py
+++ main.py
@@ -1,14 +1,43 @@
 from flask import Flask
 from flask import url_for
 from flask import render_template
+from flask import request
 from get_new_video import get_new_video
+#from rotate_background import rotate_background
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
+	#img = rotate_background.py
 	return render_template('index.html')
+
+
+@app.route('/video', methods=['POST'])
+def settings():
+	limt = request.form['limit']
+	query = request.form['query']
+	# upduration = request.form['upduration']
+	# loduration = request.form['loduration']
+	# category = request.form['category']
+	# uprating = request.form['uprating']
+	# lorating = request.form['lorating']
+	# uplikes = request.form['uplikes']
+	# lolikes = request.form['lolikes']
+	# updislikes = request.form['updislikes']
+	# lodislikes = request.form['lodislikes']
+	id = get_new_video()
+	return render_template('video.html', id=id, limit=limt, query=query)
+															# upduration=upduration,
+															# loduration=loduration,
+															# category=category,
+															# uprating=uprating,
+															# lorating=lorating,
+															# uplikes=uplikes,
+															# lolikes=lolikes,
+															# updislikes=updislikes,
+															# lodislikes=lodislikes)
 
 
 @app.route('/video')
@@ -22,4 +51,7 @@ with app.test_request_context():
 	print(url_for('static', filename='style.css'))
 
 if __name__ == '__main__':
+	# HEY! DEBUG IS TURNED ON REMEMBER TO TURN IT OFF WHEN YOU LEAVE!!!
+	app.debug = True
+	#DON'T YOU DARE FORGET IT
 	app.run()
