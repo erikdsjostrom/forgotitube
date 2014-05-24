edd79ef2163f98b08ce76959bc1dc1c3789db7a4
Erik Sjöström <metallicgeek@gmail.com>
2014-05-24 21:53:41 +0200

    fixed so that you can access the video page without initializing the main page

--- main.py
+++ main.py
@@ -6,8 +6,10 @@ from get_new_video import get_new_video
 from rotate_background import rotate_background
 
 app = Flask(__name__)
-
+global id
+id = 'ay-FQAafeR0'
 global img
+img = '../static/img/1.jpg'
 global glimit
 glimit = 'Default: 50'  # Start value
 global gquery
@@ -22,21 +24,25 @@ def index():
 	return render_template('index.html', img=img)
 
 
-@app.route('/video', methods=['POST'])
+@app.route('/video', methods=['POST', 'GET'])
 def video():
 	global img
 	global glimit
 	global gquery
-	#id = get_new_video() TAKE NOTE
-	limit = request.form['limit']
-	#Check if the limit has been updated and changed from the previous limit
-	if limit and limit != glimit:
-		glimit = limit
-	query = request.form['query']
-	if query and query != gquery:
-		gquery = query
-	id = 'ay-FQAafeR0'
-	return render_template('video.html', id=id, img=img, limit=glimit, query=gquery)
+	global id
+	#id = get_new_video()  # Sparar tid om kommenterad
+	id = 'ay-FQAafeR0'  # Statisk id
+	if request.method == 'POST':
+		limit = request.form['limit']
+		#Check if the limit has been updated and changed from the previous limit
+		if limit and limit != glimit:
+			glimit = limit
+		query = request.form['query']
+		if query and query != gquery:
+			gquery = query
+		return render_template('video.html', id=id, img=img, limit=glimit, query=gquery)
+	else:
+		return render_template('video.html', id=id, img=img, limit=glimit, query=gquery)
 
 with app.test_request_context():
 	print(url_for('index'))
