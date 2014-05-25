63fe28cd0096e29c34fd2440d4ac6fde44576157
Erik Sjöström <metallicgeek@gmail.com>
2014-05-25 02:48:54 +0200

    put everything in place so that all the different options will work

--- main.py
+++ main.py
@@ -6,43 +6,98 @@ from get_new_video import get_new_video
 from rotate_background import rotate_background
 
 app = Flask(__name__)
-global id
-id = 'ay-FQAafeR0'
+# global id
+# id = 'ay-FQAafeR0'
 global img
 img = '../static/img/1.jpg'
-global glimit
-glimit = 'Default: 50'  # Start value
-global gquery
-gquery = 'Default: random word'  # Start value
+global user_data
+user_data = ['Default: 50', 'Default: random word', "Default: ∞", "Default: 0",
+													"any", "Default: 5", "Default: 0",
+													"Default: ∞", "Default: 0",
+													"Default: ∞", "Default: 0"]
 
 
 @app.route('/')
 def index():
 	global img
 	img = rotate_background()
-	print(img)
+	print(img)  # This should probably not be here in final version
 	return render_template('index.html', img=img)
 
 
 @app.route('/video', methods=['POST', 'GET'])
 def video():
 	global img
-	global glimit
-	global gquery
+	global user_data
 	global id
-	#id = get_new_video()  # Sparar tid om kommenterad
+	#user_data = [""]*4 + ["any"] + [""]*6
+	#id = get_new_video(user_data)  # Sparar tid om kommenterad
 	id = 'ay-FQAafeR0'  # Statisk id
 	if request.method == 'POST':
+		#Perhaps a condition to skip all of this if user_input hasn't changed
 		limit = request.form['limit']
-		#Check if the limit has been updated and changed from the previous limit
-		if limit and limit != glimit:
-			glimit = limit
+		if limit and limit != user_data[0]:
+			user_data[0] = limit
 		query = request.form['query']
-		if query and query != gquery:
-			gquery = query
-		return render_template('video.html', id=id, img=img, limit=glimit, query=gquery)
+		if query and query != user_data[1]:
+			user_data[1] = query
+		upduration = request.form['upduration']
+		if upduration and upduration != user_data[2]:
+			user_data[2] = upduration
+		loduration = request.form['loduration']
+		if loduration and loduration != user_data[3]:
+			user_data[3] = loduration
+		category = request.form['category']
+		if category and category != user_data[4]:
+			user_data[4] = category
+		uprating = request.form['uprating']
+		if uprating and uprating != user_data[5]:
+			user_data[5] = uprating
+		lorating = request.form['lorating']
+		if lorating and lorating != user_data[6]:
+			user_data[6] = lorating
+		uplikes = request.form['uplikes']
+		if uplikes and uplikes != user_data[7]:
+			user_data[7] = uplikes
+		lolikes = request.form['lolikes']
+		if lolikes and lolikes != user_data[8]:
+			user_data[8] = lolikes
+		updislikes = request.form['updislikes']
+		if updislikes and updislikes != user_data[9]:
+			user_data[9] = updislikes
+		lodislikes = request.form['lodislikes']
+		if lodislikes and lodislikes != user_data[10]:
+			user_data[10] = lodislikes
+		user_data = [user_data[0], user_data[1], user_data[2],
+			user_data[3], user_data[4], user_data[5], user_data[6],
+			user_data[7], user_data[8], user_data[9], user_data[10]]
+		print(user_data)
+		#id = get_new_video(user_data)  # Sparar tid om kommenterad
+		return render_template('video.html', id=id, img=img,
+													limit=user_data[0],
+													query=user_data[1],
+													upduration=user_data[2],
+													loduration=user_data[3],
+													category=user_data[4],
+													uprating=user_data[5],
+													lorating=user_data[6],
+													uplikes=user_data[7],
+													lolikes=user_data[8],
+													updislikes=user_data[9],
+													lodislikes=user_data[10])
 	else:
-		return render_template('video.html', id=id, img=img, limit=glimit, query=gquery)
+		return render_template('video.html', id=id, img=img,
+													limit=user_data[0],
+													query=user_data[1],
+													upduration=user_data[2],
+													loduration=user_data[3],
+													category=user_data[4],
+													uprating=user_data[5],
+													lorating=user_data[6],
+													uplikes=user_data[7],
+													lolikes=user_data[8],
+													updislikes=user_data[9],
+													lodislikes=user_data[10])
 
 with app.test_request_context():
 	print(url_for('index'))
