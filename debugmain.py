1cad9e256450db34c3a56fb034e64ee3044bb772
Erik Sjöström <metallicgeek@gmail.com>
2014-05-25 15:56:51 +0200

    removed some options

--- debugmain.py
+++ debugmain.py
@@ -12,9 +12,7 @@ global img
 img = '../static/img/1.jpg'
 global user_data
 user_data = ['Default: 50', 'Default: random word', "Default: ∞", "Default: 0",
-													"any", "Default: 5", "Default: 0",
-													"Default: ∞", "Default: 0",
-													"Default: ∞", "Default: 0"]
+													"any"]
 
 
 @app.route('/')
@@ -73,7 +71,7 @@ def video():
 		# user_data = [user_data[0], user_data[1], user_data[2],
 		# 	user_data[3], user_data[4], user_data[5], user_data[6],
 		# 	user_data[7], user_data[8], user_data[9], user_data[10]]
-		user_data = [10000] + ['erik'] + [10000000] + [0] + ["any"] + [""]*5 + [1]
+		user_data = [10000] + ['erik'] + [10000000] + [0] + ["any"]
 		print(user_data[0])
 		id = get_new_video(user_data)  # Sparar tid om kommenterad
 		return render_template('video.html', id=id, img=img,
@@ -81,29 +79,17 @@ def video():
 													query=user_data[1],
 													upduration=user_data[2],
 													loduration=user_data[3],
-													category=user_data[4],
-													uprating=user_data[5],
-													lorating=user_data[6],
-													uplikes=user_data[7],
-													lolikes=user_data[8],
-													updislikes=user_data[9],
-													lodislikes=user_data[10])
+													category=user_data[4])
 	else:
 		print('ELSE')
-		user_data = [""]*4 + ["any"] + [""]*6
+		user_data = [""]*4 + ["any"]
 		id = get_new_video(user_data)  # Sparar tid om kommenterad
 		return render_template('video.html', id=id, img=img,
 													limit=user_data[0],
 													query=user_data[1],
 													upduration=user_data[2],
 													loduration=user_data[3],
-													category=user_data[4],
-													uprating=user_data[5],
-													lorating=user_data[6],
-													uplikes=user_data[7],
-													lolikes=user_data[8],
-													updislikes=user_data[9],
-													lodislikes=user_data[10])
+													category=user_data[4])
 
 with app.test_request_context():
 	print(url_for('index'))
